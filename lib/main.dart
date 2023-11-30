2e9b65380126e059934b0ecc7b5c5f12ed678b07
Fahim Hoque <fahimhoque@doinkmaster.local>
2023-11-30 12:45:56 +0600

    firebase cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:secured_image_vault/app.dart';
+import 'package:secured_image_vault/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: FirebaseOptions(
-        apiKey: "AIzaSyA9hDMH1ocNwBIaq6sklcU74xDfLVMbURE",
-        appId: "1:934229348374:android:52e5629e03f74e0590e537",
-        messagingSenderId: "934229348374",
-        projectId: "imagevaul"),
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
